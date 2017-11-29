5caf765bdcd3cd1160fadbe8b92330502be68355
ga15 <manas.dash@in.tesco.com>
2017-11-29 07:55:33 +0530

    Read integers from a file and perform operations

---
+++ src/main/java/ReadIntegerFromAFile.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.File;
+
+/**
+ * Created by Manas Dash on 29/11/17 7:29 AM.
+ * 100DaysOFCode
+ * Reading integer from a file
+ */
+class ReadIntegerFromAFile {
+
+
+    private File GetFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource("SampleFile.txt").getFile());
+    }
+
+    private int ReadIntegerAndSum() {
+        int sum = 0;
+        try {
+
+            Scanner input;
+            input = new Scanner(
+                    new BufferedReader(
+                            new FileReader(this.GetFile())));
+            while (input.hasNext()) {
+                if (input.hasNextInt()) {
+                    sum += input.nextInt();
+                } else {
+                    input.next();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ReadIntegerFromAFile rif = new ReadIntegerFromAFile();
+        System.out.println(rif.ReadIntegerAndSum());
+    }
+}

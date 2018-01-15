53e4ca8c6dca32100837cc2bcc36d2ed47102e76
ManasDash <simplymanas@gmail.com>
2018-01-15 16:33:37 +0530

    Even number test Prime number test

---
+++ src/main/java/EvenNumberTest.java
@@ -0,0 +1,24 @@
+public class EvenNumberTest {
+    public static void main(String[] args) {
+        int number = 5;
+        int finishNumber = 30;
+        while(number <= finishNumber) {
+            if (!IsEvenNumber(number)) {
+                number++;
+                continue;
+            }
+
+            System.out.println("even no:" + number);
+            number++;
+        }
+    }
+
+    private static  boolean IsEvenNumber(int number){
+        if(number%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

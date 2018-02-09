70cba362851834db2635aa66d138a36ae184e8e3
ManasDash <simplymanas@gmail.com>
2018-02-09 06:40:00 +0530

    Refactoring simple HTTP Server (by Sundar)

--- src/main/java/com/simplymanas/learning/HttpServerImp/SimplestHTTPServer.java
+++ src/main/java/com/simplymanas/learning/HttpServerImp/SimplestHTTPServer.java
@@ -6,16 +6,15 @@ import java.net.Socket;
 public class SimplestHTTPServer {
 
     public static void main(String[] args) throws Exception {
-        ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080 ....");
 
         int count = 0;
         do {
-            try (Socket socket = server.accept()) {
-                System.out.println("listening " + count);
-                System.out.println(socket.getInetAddress().toString());
-
-                count++;
+            try (Socket socket = (new ServerSocket(8080)).accept()) {
+                System.out.println(
+                        "Listening for connection on port 8080 ...."
+                                + count++
+                                + " "
+                                + socket.getInetAddress().toString());
             }
         } while (true);
     }
